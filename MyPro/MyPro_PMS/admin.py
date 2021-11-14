5faea0d4d25ac075e2b8ada0f3c0f415b6e135c7
Austin May <austin.may@protonmail.com>
2021-11-14 15:41:34 -0500

    Registered models for viewing in Django Admin

--- MyPro/MyPro_PMS/admin.py
+++ MyPro/MyPro_PMS/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Designation)
+admin.site.register(Profile)
+admin.site.register(Client)
+admin.site.register(Technology)
+admin.site.register(EmployeeAccess)
+admin.site.register(Knowledge)
+admin.site.register(Employee)
+admin.site.register(Project)
